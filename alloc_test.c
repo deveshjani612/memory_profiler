9abceeab10f02f705647401b2daf534295c3e987
deveshjani612 <devesh.h.jani@gmail.com>
2025-11-07 17:32:05 -0500

    Enhance memory allocation test with more writes
    Added additional memory writes and increased sleep duration.

--- alloc_test.c
+++ alloc_test.c
@@ -6,9 +6,12 @@ int main() {
         void *p = mmap(NULL, 4096 * 100, PROT_READ | PROT_WRITE,
                        MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
         ((char*)p)[0] = 1;
-        usleep(500000);
+        ((char*)p)[4096] = 1;
+        ((char*)p)[4096*2] = 1;
+        ((char*)p)[4096*3] = 1;
+        ((char*)p)[4096*4] = 1;
+        sleep(2);
         munmap(p, 4096 * 100);
     }
     return 0;
 }
-
